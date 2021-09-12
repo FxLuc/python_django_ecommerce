142c344ae91176576821ac5b6f72007b3e13b7e6
FlexAlderson <phantoinang2001@gmail.com>
2021-09-12 16:26:44 +0700

    create database

--- django_ecommerce_app/models.py
+++ django_ecommerce_app/models.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 
 class CustomUser(AbstractUser):
     user_type_choices = ((1, "Admin"), (2, "Staff"), (3, "Merchant"), (4, "Customer"))
-    user_type = models.CharField(max_length=255, choices=user_type_choices, default=1)
+    user_type = models.CharField(max_length=150, choices=user_type_choices, default=1)
 
 
 class AdminUser(models.Model):
@@ -27,8 +27,8 @@ class StaffUser(models.Model):
 class MerchantUser(models.Model):
     auth_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     profile_pic = models.FileField(default="")
-    company_name = models.CharField(max_length=255)
-    gst_details = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=150)
+    gst_details = models.CharField(max_length=150)
     address = models.TextField()
     is_added_by_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -43,8 +43,8 @@ class CustomerUser(models.Model):
 
 class Categories(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    url_slug = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
+    url_slug = models.CharField(max_length=150)
     thumbnail = models.FileField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -60,8 +60,8 @@ class Categories(models.Model):
 class SubCategories(models.Model):
     id = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    url_slug = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
+    url_slug = models.CharField(max_length=150)
     thumbnail = models.FileField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -73,12 +73,12 @@ class SubCategories(models.Model):
 
 class Products(models.Model):
     id = models.AutoField(primary_key=True)
-    url_slug = models.CharField(max_length=255)
+    url_slug = models.CharField(max_length=150)
     subcategories_id = models.ForeignKey(SubCategories, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=255)
-    brand = models.CharField(max_length=255)
-    product_max_price = models.CharField(max_length=255)
-    product_discount_price = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=150)
+    brand = models.CharField(max_length=150)
+    product_max_price = models.CharField(max_length=150)
+    product_discount_price = models.CharField(max_length=150)
     product_description = models.TextField()
     product_long_description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -92,7 +92,7 @@ class ProductMedia(models.Model):
     id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
     media_type_choice = ((1, "Image"), (2, "Video"))
-    media_type = models.CharField(max_length=255)
+    media_type = models.CharField(max_length=150)
     media_content = models.FileField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
@@ -104,16 +104,16 @@ class ProductTransaction(models.Model):
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
     transaction_product_count = models.IntegerField(default=1)
     transaction_type = models.CharField(
-        choices=transaction_type_choices, max_length=255)
-    transaction_description = models.CharField(max_length=255)
+        choices=transaction_type_choices, max_length=150)
+    transaction_description = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class ProductDetails(models.Model):
     id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    title_details = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
+    title_details = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
 
@@ -121,7 +121,7 @@ class ProductDetails(models.Model):
 class ProductAbout(models.Model):
     id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
 
@@ -129,7 +129,7 @@ class ProductAbout(models.Model):
 class ProductTags(models.Model):
     id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
 
@@ -149,7 +149,7 @@ class ProductReviews(models.Model):
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
     user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
     review_image = models.FileField()
-    rating = models.CharField(default="5", max_length=255)
+    rating = models.CharField(default="5", max_length=150)
     review = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
@@ -166,7 +166,7 @@ class ProductReviewVoting(models.Model):
 
 class ProductVarient(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -175,25 +175,25 @@ class ProductVarientItems(models.Model):
     product_varient_id = models.ForeignKey(
         ProductVarient, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CustomerOrders(models.Model):
     id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey(Products, on_delete=models.DO_NOTHING)
-    purchase_price = models.CharField(max_length=255)
-    coupon_code = models.CharField(max_length=255)
-    discount_amt = models.CharField(max_length=255)
-    product_status = models.CharField(max_length=255)
+    purchase_price = models.CharField(max_length=150)
+    coupon_code = models.CharField(max_length=150)
+    discount_amt = models.CharField(max_length=150)
+    product_status = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class OrderDeliveryStatus(models.Model):
     id = models.AutoField(primary_key=True)
     order_id = models.ForeignKey(CustomerOrders, on_delete=models.CASCADE)
-    status = models.CharField(max_length=255)
-    status_message = models.CharField(max_length=255)
+    status = models.CharField(max_length=150)
+    status_message = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
