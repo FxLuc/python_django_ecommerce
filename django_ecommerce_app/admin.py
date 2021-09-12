789841701cd9837ee489479f2a4ad85fc7b75396
FlexAlderson <phantoinang2001@gmail.com>
2021-09-12 16:39:43 +0700

    update admin.py

--- django_ecommerce_app/admin.py
+++ django_ecommerce_app/admin.py
@@ -1,3 +1,7 @@
+from django_ecommerce_app.models import Categories
 from django.contrib import admin
+from django_ecommerce_app.models import Categories, SubCategories
 
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(SubCategories)
