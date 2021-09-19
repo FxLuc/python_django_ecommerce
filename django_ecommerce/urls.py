a7356c2dcdf98acdd0278de9d56527d4da2fedd2
FlexAlderson <phantoinang2001@gmail.com>
2021-09-19 19:42:31 +0700

    add static and templates

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_ecommerce import settings
+from django_ecommerce_app import views
+from django.conf.urls.static import static
+from django_ecommerce import settings 
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('admin/', views.admin_login),
+    path('index', views.index)
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
